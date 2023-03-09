42685e53c603b5ad3d95c56917772d7483717556
darshit <darshit@logicrays.com>
2023-03-09 15:13:06 +0530

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import { GoalList } from './components/GoalList';
 
 function App() {  
 
   return (
     <div className="App">
-      <GoalList name={"Avscdasfsadf"} />
     </div>
   );
   
