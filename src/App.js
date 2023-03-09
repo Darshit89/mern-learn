549f489903c1c8f9eb02d6f34ead7b75647f104c
darshit <darshit@logicrays.com>
2023-03-09 14:23:18 +0530

    intial first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { GoalList } from './components/GoalList';
+
+function App() {  
+
+  return (
+    <div className="App">
+      <GoalList name={"Avscdasfsadf"} />
+    </div>
+  );
+  
+}
+
+export default App; 
